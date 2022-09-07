cd6d2fa8fdaae9e2ffdc2e01ea6b9283f92bb075
Haider Alkhalifah <haideralkhalifah@gmail.com>
2022-09-07 12:47:22 +0300

    ch almost done

--- src/pages/leftPage.js
+++ src/pages/leftPage.js
@@ -1,15 +1,11 @@
+
+import Navbaru from "../components/leftPage/navbar";
 import Termsq from "../components/leftPage/termSq";
 export default function LeftPage() {
   return (
     <div className="w-full overflow-y-auto scrollbar-hide bg-b2 h-screen p-6 gap-4 grid justify-items-center ">
-      <div className="container flex justify-between items-center mb-3">
-        <div className="container flex items-center w-full shadow-sm rounded-xl h-[90px] p-2 justify-between bg-purp text-white">
-        <h1 className="text-bold text-4xl">MyPlan</h1>
-
-          <div>143</div>
-          <div className="">CH remaining</div>
-        </div>
-      </div>
+      <Navbaru />
+      <hr className="justify-self-start rounded-full bg-textP border w-[1000px]"></hr>
       <div className="w-full grid grid-cols-4 justify-items-center gap-4 items-center">
         <Termsq />
         <Termsq />
